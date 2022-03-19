261122b206d4ca0d8c97bed6235e4a5df25c32bb
SmithWebDev <smithwebdd@gmail.com>
2022-03-19 12:56:05 -0400

    updated snippets init file

--- nvim-0.7/lua/smithwebdev/snippets/init.lua
+++ nvim-0.7/lua/smithwebdev/snippets/init.lua
@@ -1,26 +1,20 @@
 local ls       = require'luasnip'
 
 -- snippet variables --
-local snip     = ls.snippet
-local node     = ls.snippet_node
-local text     = ls.text_node
-local insert   = ls.insert_node
-local func     = ls.function_node
-local choic    = ls.choice_node
-local dynamicn = ls.dynamic_node
-local fmt = require('luasnip.extras.fmt').fmt
+local s     = ls.snippet
+local sn    = ls.snippet_node
+local isn   = ls.indent_snippet_node
+local t     = ls.text_node
+local i     = ls.insert_node
+local f     = ls.function_node
+local c     = ls.choice_node
+local d     = ls.dynamic_node
+local r     = ls.restore_node
+local event = require('luasnip.util.events')
+local fmt   = require('luasnip.extras.fmt').fmt
 
-local date = function() return { os.date('%Y-%m-%d') } end
+local date  = function() return { os.date('%Y-%m-%d') } end
 
---ls.snippets = {
---  all = {
---    snip({
---      trig = 'date',
---      name = 'Date',
---      dscr = 'Date in the form of YYYY-MM-DD'
---    }),
---  }
---}
 print('snippets installed!')
 
 
