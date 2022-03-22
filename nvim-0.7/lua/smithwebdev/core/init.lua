b559b3e3294dcf0b1e299e435e97ba1d6ba95764
SmithWebDev <smithwebdd@gmail.com>
2022-03-22 14:01:11 -0400

    began working on config repo autocmds

--- nvim-0.7/lua/smithwebdev/core/init.lua
+++ nvim-0.7/lua/smithwebdev/core/init.lua
@@ -1,4 +1,4 @@
 require 'smithwebdev.core.utils'
 require 'smithwebdev.core.settings'
 require 'smithwebdev.core.keybinds'
---require 'smithwebdev.core.autocmds'
+require 'smithwebdev.core.autocmds'
--- nvim-0.7/lua/smithwebdev/snippets/init.lua
+++ nvim-0.7/lua/smithwebdev/snippets/init.lua
@@ -27,9 +27,41 @@ ls.snippets = {
 
   lua = {
     s("req", fmt("local {} = require('{}')", { i(1), rep(1)})),
-    s('pcreate', fmt("local M = {{}},\n\nM.plugin = {{\n  '{}',\n\n  config = function()\n    {}\n  end\n}}\n\nreturn M",  {i(1), i(2)})),
-    s('keymap', fmt("{}noremap('{}',  '{}', {{ desc = '{}'}})", {i(1), i(2), i(3), i(4)}))
-  },
-}
+    s('keymap', fmt("{}noremap('{}',  '{}', {{ desc = '{}'}})", {i(1), i(2), i(3), i(4)})),
+    s('pcreate', fmt([[
+    local M = {{}},
 
-print('snippets installed!')
+    M.plugin = {{
+      '{}',
+
+      config = function()
+        {}
+      end
+    }}
+
+    return M]],
+    {i(1), i(2)})),
+
+    --s("auc", fmt("vim.api.nvim_create_autocmd('{}')", i(1))),
+
+    --s('auc', fmt([[
+    --  vim.api.nvim_create_autocmd('{}', {
+    --    pattern = '{}',
+    --    command = '{}'{}
+    --    }), {i(1), i(2), i(3), c(4, sn(nil, fmt(",\n  group = {}"), i(0))), t""}]]))
+
+    s('auc', fmt(
+    [[vim.api.nvim_create_autocmd('{}', {{
+      pattern = '{}',
+      command = '{}'
+    }})]], 
+    {i(1), i(2), i(3)})),
+
+    s(agroup, c(1, {
+      t("some text here"),
+      i(nil, 'Heres something else'),
+      t("this closes out the choices") }))
+    }
+  }
+
+  print('snippets installed!')
