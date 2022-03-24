776e23a906febf9bbc6b84a818273b8e7c890ac8
SmithWebDev <smithwebdd@gmail.com>
2022-03-23 21:38:08 -0400

    updated autocmd file so test commands no longer triggers

--- nvim-0.7/lua/smithwebdev/core/autocmds.lua
+++ nvim-0.7/lua/smithwebdev/core/autocmds.lua
@@ -1,6 +1,5 @@
 -- local AutoSave = vim.api.nvim_create_augroup('AutoSave', { clear = true })
---vim.api.nvim_create_autocmd("BufEnter", { command = "echo 'Hello'", group = AutoSave})
-
+-- vim.api.nvim_create_autocmd("BufEnter", { command = "echo 'Hello'", group = AutoSave})
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'help',
   command = 'wincmd L'
