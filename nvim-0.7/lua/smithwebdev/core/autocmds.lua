d6122ca1f54f6cf0aaf295e7e3458684bc2f36cd
Darrick Smith <smithwebdd@gmail.com>
2022-03-28 12:01:03 -0400

    added raw autocmd for autosave, corrected typo in auc snippet

--- nvim-0.7/lua/smithwebdev/core/autocmds.lua
+++ nvim-0.7/lua/smithwebdev/core/autocmds.lua
@@ -1,6 +1,17 @@
 -- local AutoSave = vim.api.nvim_create_augroup('AutoSave', { clear = true })
 -- vim.api.nvim_create_autocmd("BufEnter", { command = "echo 'Hello'", group = AutoSave})
+
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'help',
   command = 'wincmd L'
   })
+
+local auto = vim.cmd
+auto [[
+    augroup autosave
+        au!
+        let blacklist = ['packer', 'netrw', 'TelescopePrompt', 'lspinfo', 'lsp-installer', 'query', 'tsplayground', 'text', 'harpoon', 'scratch']
+        au BufEnter * if &filetype == "" | setlocal ft=text | endif
+        au TextChanged,InsertLeave * if index(blacklist, &ft) < 0 && &modifiable && &buftype == ''| silent w | endif
+    augroup END
+  ]]
