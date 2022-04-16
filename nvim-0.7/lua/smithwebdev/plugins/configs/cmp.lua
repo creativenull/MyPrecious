989eb8ff5ada529be087fdccaecf2eac86cee000
SmithWebDev <smithwebdd@gmail.com>
2022-04-16 00:12:38 -0400

    updated cmp file due to breaking changes (old file renamed xcmp)

--- nvim-0.7/lua/smithwebdev/plugins/configs/cmp.lua
+++ nvim-0.7/lua/smithwebdev/plugins/configs/cmp.lua
@@ -10,14 +10,21 @@ M.plugin = {
     'octaltree/cmp-look',                  -- https://github.com/octaltree/cmp-look
     'saadparwaiz1/cmp_luasnip',            -- https://github.com/saadparwaiz1/cmp_luasnip
     'hrsh7th/cmp-nvim-lsp',                -- https://github.com/hrsh7th/cmp-nvim-lsp
+    'hrsh7th/cmp-nvim-lsp-signature-help', -- https://github.com/hrsh7th/cmp-nvim-lsp-signature-help
     'hrsh7th/cmp-nvim-lua',                -- https://github.com/hrsh7th/cmp-nvim-lua
     'quangnguyen30192/cmp-nvim-tags',      -- https://github.com/quangnguyen30192/cmp-nvim-tags
     'hrsh7th/cmp-path',                    -- https://github.com/hrsh7th/cmp-path
     'ray-x/cmp-treesitter',                -- https://github.com/ray-x/cmp-treesitter
+    'hrsh7th/cmp-emoji',                   -- https://github.com/hrsh7th/cmp-emoji
+    'nvim-orgmode/orgmode',                -- https://github.com/nvim-orgmode/orgmode
+    'jc-doyle/cmp-pandoc-references',      -- https://github.com/jc-doyle/cmp-pandoc-references
+    'aspeddro/cmp-pandoc.nvim',            -- https://github.com/aspeddro/cmp-pandoc.nvim
 
     -- Additional 
     'onsails/lspkind-nvim',                -- https://github.com/onsails/lspkind-nvim},
     'windwp/nvim-autopairs',               -- https://github.com/windwp/nvim-autopairs
+    'nvim-lua/plenary.nvim',
+    'jbyuki/nabla.nvim',
 
     -- Premade Snippet Plugins
     'honza/vim-snippets',                  -- https://github.com/honza/vim-snippets
@@ -27,13 +34,13 @@ M.plugin = {
   config = function()
     local cmp = require('cmp')
     local lspkind = require('lspkind')
-    local luasnip = require('luasnip')
+    local luasnip = require("luasnip")
     local u = require("smithwebdev.core.utils")
     local trig = u.trigger_completion
 
     lspkind.init()
 
-    local has_any_words_before = function()
+    local has_words_before = function()
       if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
         return false
       end
@@ -41,94 +48,120 @@ M.plugin = {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
 
-    --local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    --cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
-
-    cmp.setup.cmdline(
-    ':', {
-      sources = {
-        { name = 'cmdline' }
-      },
-    },
-    '/', {
-      sources = {
-        { name = 'buffer' }
-      }
-    })
-
     cmp.setup({
       snippet = {
         expand = function(args)
-          local luasnip = require('luasnip')
-          if not luasnip then
-            return
-          end
-          require('luasnip').lsp_expand(args.body)
+          require("luasnip").lsp_expand(args.body)
         end,
       },
-      mapping = {
-        ["<c-j>"] = cmp.mapping(function(fallback)
+
+      window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+      },
+
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-j>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
           elseif luasnip.expand_or_jumpable() then
             luasnip.expand_or_jump()
-          elseif has_words_before() then
+          elseif has_words_before then
             cmp.complete()
           else
             fallback()
           end
-        end, {"i", "s" }),
-
-        ["<c-k>"] = cmp.mapping(function(fallback)
+        end, {'i', 's'}),
+        ['<C-k>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_prev_item()
+            cmp.select_next_item()
           elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
           end
-        end, {"i", "s" }),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["jk"] = cmp.mapping.confirm {
-          behavior = cmp.ConfirmBehavior.Insert,
-          select = true
-        },
-        ["<c-Space>"] = cmp.mapping.complete()
-      },
-      sources = {
-        { name = 'luasnip'},
-        { name = 'tags'},
-        { name = 'nvim_lsp'},
-        { name = 'nvim_lua'},
+        end, {'i', 's'}),
+        --['C-Space'] = cmp.mapping.complete,
+        ['<C-e>'] = cmp.mapping.close(),
+        ['jk'] = cmp.mapping.confirm({ select = true })
+      }),
+
+      sources = cmp.config.sources({
+        {name = 'LSP'},
+        {name = 'luasnip'},
+        {name = 'emoji'},
+        {name = 'orgmode'},
+        {name = 'pandoc_references'},
+        {name = 'cmp-pandoc.nvim'},
+      }, {
         { name = 'buffer', keyword_length = 5 },
-        { name = 'path'},
-        { name = 'orgmode'},
-      },
+        { name = 'nvim_lsp_signature_help' },
+      }),
+
       formatting = {
-        format       = lspkind.cmp_format({
-          with_text  = false,
-          maxwidth   = 50,
-          menu       = ({
-            luasnip  = '[SNIP]',
-            nvim_lsp = '[LSP]',
-            tags     = '[TAGS]',
-            nvim_lua = '[LUA]',
+        format = lspkind.cmp_format({
+          mode = 'symbol_text',
+          menu = ({
+            luasnip  = "[SNIP]",
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[LUA]",
+            tags     = "[TAGS]",
             buffer   = '[BUFFER]',
             path     = '[PATH]',
-            orgmode  = '[ORGMODE]'
-          }),
+            emoji    = '[EMOJI]',
+            orgmode  = '[ORG]',
+            pandoc   = '[PDOC]'
+          })
         }),
-        experimental = {
-          native_menu = false,
-          ghost_text = false,
-        },
       },
     })
-  end
 
-  --inoremap('<CR>', trig)
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer', keyword_length = 5  }
+      },
+      view = {
+        entries = { name = 'wildmenu', separator = '|' }
+      }
+    })
+
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' }
+      }, {
+        { name = 'cmdline' }
+      }),
+      view = {
+        entries = { name = 'wildmenu', separator = '|' }
+      }
+    })
+
+    -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    -- require('lspconfig')['place_server_name_here'].setup {
+    --   capabilities = capabilities
+    -- }
+
+    -- " gray
+    -- highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+    -- " blue
+    -- highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+    -- highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+    -- " light blue
+    -- highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+    -- highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+    -- highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+    -- " pink
+    -- highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+    -- highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+    -- " front
+    -- highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+    -- highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+    -- highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
+  end
 }
 
 return M
